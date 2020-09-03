bc4d7198e6daac536d91616e9399683b6e439950
Carrie Grossman <carriegrossman@MacBook-Pro.local>
2020-09-03 15:12:41 -0400

    added build

---
+++ build/precache-manifest.bb83c14f76a04ec576af5757eff25692.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "608cf7e6f333c30fa02e1507eed54ef8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97b2b4f2fbc3535bc029",
+    "url": "/static/css/main.7f1770ee.chunk.css"
+  },
+  {
+    "revision": "432b25b2bd70647f25e5",
+    "url": "/static/js/2.07a666ac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.07a666ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97b2b4f2fbc3535bc029",
+    "url": "/static/js/main.89b684e3.chunk.js"
+  },
+  {
+    "revision": "eecdb62e7665c0993daf",
+    "url": "/static/js/runtime-main.e8d921e4.js"
+  },
+  {
+    "revision": "d6b4f7a8075c76936346517794fa867a",
+    "url": "/static/media/cocktailbaby.d6b4f7a8.jpg"
+  }
+]);
